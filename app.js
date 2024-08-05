a7463cb2eddb233d58c366520b24a12630a226f8
gremoar <116038475+gremoar@users.noreply.github.com>
2024-08-05 18:47:51 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const modalNota = new bootstrap.Modal(document.querySelector('#modalNota'), {})
 const modalNotaB = new bootstrap.Modal(document.querySelector('#modalNotaB'), {})
-const modalNotaPL = new bootstrap.Modal(document.querySelector('#modalNotaPL'), {})
+const modalNotaC = new bootstrap.Modal(document.querySelector('#modalNotaC'), {})
+
+
 let nota = document.querySelector('#nota');
 let notaB = document.querySelector('#notaB');
-let notaPL = document.querySelector('#notaPL');
+let notaC = document.querySelector('#notaC');
 
 
-let listaPlaylist = document.querySelector('#listaPlaylist');
 let listaNotasA = document.querySelector('#listaNotasA');
 let listaNotasB = document.querySelector('#listaNotasB');
 let listaNotasC = document.querySelector('#listaNotasC');
-let listaNotasD = document.querySelector('#listaNotasD');
-
 let sNombre = document.querySelector('#sNombre');
 let sContador = document.querySelector('#sContador');
 
@@ -26,9 +25,10 @@ function abrirModal(){
 function abrirModalB(){
     modalNotaB.show();
 }
-function abrirModalPL(){
-    modalNotaPL.show();
+function abrirModalC(){
+    modalNotaC.show();
 }
+
 
 function cerrar(){
     modalNota.hide();
@@ -37,6 +37,9 @@ function cerrar(){
 function cerrar(){
     modalNotaB.hide();
 }
+function cerrar(){
+    modalNotaC.hide();
+}
 
 function borrar(li){
     if( confirm('Seguro que vas a eliminar la nota?')){
@@ -44,8 +47,11 @@ function borrar(li){
         li.parentNode.parentNode.remove();
         let datosA = listaNotasA.innerHTML;
         let datosB = listaNotasB.innerHTML;
+        let datosC = listaNotasC.innerHTML;
 
-        localStorage.setItem('notas', datosA, datosB);
+
+
+        localStorage.setItem('notas', datosA, datosB, datosC);
 
         cantidad--;
         sContador.innerText = cantidad;
@@ -93,67 +99,34 @@ function guardar2(){
     localStorage.setItem('notas', datosB);
     modalNotaB.hide();
 }
+function guardarC(){
+    let textoC = notaC.value;
 
-
-function guardarPL(){
-    let textoPL = notaPL.value;
-
-    console.log(textoPL);
-    listaPlaylist.innerHTML += 
-    `<section>
-            <div class="boxes">
-                <h1 class="name">Playlist 3</h1>
-                <details>
-                <summary> ${textoPL} </summary>
-                <ul id="listaNotas" class="list-group">
-                    
-                </ul>
-                <button onclick="abrirModal()" type="button" class="btn btn-success btn-nueva"><i class="fa-solid fa-plus"></i> </button>
-                </details>
-        </div>
-        </section>`;
+    console.log(textoC);
+    listaNotasC.innerHTML += 
+    `<li class="list-group-item d-flex justify-content-between p-2"> ${textoC}
+        <span class="d-flex gap-2">
+            <i class="text-danger fa-solid fa-trash" onclick="borrar(this)"></i> 
+        </span>
+    </li>`;
     nota.value = '';
     cantidad++;
     sContador.innerText = cantidad
-    let datosPl = listaPlaylist.innerHTML;
-    localStorage.setItem('notas', datosPl);
-    modalNotaPL.hide();
+    let datosC = listaNotasC.innerHTML;
+    localStorage.setItem('notas', datosC);
+    modalNotaC.hide();
 }
-
-function guardarPL1(){
-    let textoPL = notaPL.value;
-
-    console.log(textoPL);
-    listaPlaylist.innerHTML += 
-    `<section>
-            <div class="boxes">
-                <h1 class="name">Playlist 3</h1>
-                <details>
-                <summary> ${textoPL} </summary>
-                <ul id="listaNotas" class="list-group">
-                    
-                </ul>
-                <button onclick="abrirModal()" type="button" class="btn btn-success btn-nueva"><i class="fa-solid fa-plus"></i> </button>
-                </details>
-        </div>
-        </section>`;
-    nota.value = '';
-    cantidad++;
-    sContador.innerText = cantidad
-    let datosPl = listaPlaylist.innerHTML;
-    localStorage.setItem('notas', datosPl);
-    modalNotaPL.hide();
-}
-
-
 
 // Guarda en favoritos y cambiamos la clase
 function favorito( i ){
     let texto = i.parentNode.parentNode.innerText;
     let textoB = i.parentNode.parentNode.innerText;
+    let textoC = i.parentNode.parentNode.innerText;
     let nota = i.parentNode.parentNode;
     let notaB = i.parentNode.parentNode;
-    console.log(nota, texto, notaB, textoB);
+    let notaC = i.parentNode.parentNode;
+
+    console.log(nota, texto, notaB, textoB, notaC, textoC);
     favoritos.push( texto )
     i.classList.toggle('fa-regular');
     i.classList.toggle('fa-solid');
@@ -161,7 +134,6 @@ function favorito( i ){
     ulFavoritos.innerHTML += 
     `<li class="list-group-item d-flex justify-content-between p-2"> ${texto}
         <i class="fa-solid fa-xmark" onclick="borrarFavorito(this)"></i>    
-
     </li>`
 }
 
@@ -169,10 +141,8 @@ function favorito( i ){
 function leer(){
     let datosA = localStorage.getItem('notas');
     let datosB = localStorage.getItem('notas');
-    listaNotasB.innerHTML = datosA;
+    let datosC = localStorage.getItem('notas');
+    listaNotasA.innerHTML = datosA;
     listaNotasB.innerHTML = datosB;
-
+    listaNotasC.innerHTML = datosC;
 }
-
-
-
